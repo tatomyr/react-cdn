5086478b38a015ba85c1cc840f8c273e09f46387
tatomyr <andrew.tatomyr@gmail.com>
2016-10-13 00:59:20 +0300

    Language dynamically changing

--- app/react-components.js
+++ app/react-components.js
@@ -1,7 +1,20 @@
 const MainLayout = React.createClass({
+  getInitialState: function() {
+    return { lang: undefined };
+  },
+
+  setLanguage: function(event) {
+    console.log(event.currentTarget.id);
+    this.setState({ lang: event.currentTarget.id });
+  },
+
   render: function() {
     return (<div>
-      {{ en: `Hello`, uk: `Привіт`, es: `Hola` }['uk']}
+      <h1>React test</h1>
+      <input type="button" id="en" onClick={this.setLanguage} value="[EN]" />
+      <button id="uk" onClick={this.setLanguage}>[UK]</button>
+      <hr />
+      {{ en: `Hello`, uk: `Привіт`, es: `Hola` }[this.state.lang]}
     </div>);
   }
 });
