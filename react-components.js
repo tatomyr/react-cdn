01c410df82ae79968851d7e7613a518dfd16f081
tatomyr <andrew.tatomyr@gmail.com>
2016-10-12 13:47:52 +0300

    Prototype (working with server)

---
+++ react-components.js
@@ -0,0 +1,9 @@
+var MainLayout = React.createClass({
+  render: function() {
+    return (<div>
+      {{ en: `Hello`, uk: `Привіт`, es: `Hola` }['uk']}
+    </div>);
+  }
+});
+
+ReactDOM.render(<MainLayout />, document.getElementById('react-root'));
